2e71449c61afade051285e8740bf2bc84dc9f63a
Freek Kalter <freek@kalteronline.org>
2012-12-10 23:03:36 +0100

    looser input posible, color matched part

--- check.pl
+++ check.pl
@@ -1,14 +1,12 @@
-#!/usr/bin/perl 
+#!/usr/bin/env perl
 #===============================================================================
 #
 #         FILE: check.pl
 #
 #        USAGE: ./check.pl ticket-number
 #
-#  DESCRIPTION: 
 #
 #       AUTHOR: Freek Kalter
-#      COMPANY: 
 #      VERSION: 0.1
 #      CREATED: 10/10/2012 08:45:44 PM
 #===============================================================================
@@ -20,14 +18,15 @@ use HTML::TreeBuilder;
 use LWP;
 use Encode;
 use Data::Dumper;
+use Term::ANSIColor;
 
 binmode(STDOUT, ":utf8");
 
-my $number = shift;
-chomp $number;
-if( $number !~ m/^[A-Z]{2}[0-9]{6}$/ ){
+chomp( my $number = uc($ARGV[1] ? "$ARGV[0]$ARGV[1]" : $ARGV[0] ));
+if( $number !~ m/^[A-Za-z]{2} ?[0-9]{6}$/ ){
     die "Usage: check.pl CI005406";
 }
+
 my @number = split('', $number);
 
 my $url = "https://www.staatsloterij.nl/trekkingsuitslag";
@@ -37,25 +36,28 @@ $root->parse($content);
 
 my @prizes =();
 my $prize = 0;
+my $matched ="";
 for my $tr ( $root->find_by_attribute( "class" , "nr$number[-1]" )->look_down( "_tag", "tr" ) ){
     for my $td ($tr->find_by_tag_name("td")){
         if($td->attr("class") eq "hCol2"){
             my ($let, $num) = $td->as_text =~ m/([A-Z]{0,2})\s+([0-9]{1,6})/;
             if( $number =~ /$let$num$/ ){
-                say "matched: $let$num";
+                #say "matched: $let$num";
                 my ($money) = $td->right()->as_text =~ m/([0-9.,]+)/;
                 $money =~ s/[.,]//g;
-                if(  $money > $prize ) { $prize = $money } ;
+                if(  $money > $prize ) { $prize = $money; $matched="$let$num"; } ;
             }
         }
     }
 }
+print substr $number, 0, length($matched)* -1;
+say colored($matched, 'red bold');
 if($prize > 0){
     $prize =~ s/(.*)([0-9]{2})$/$1,$2/; # add , on right place
     1 while $prize =~ s/(\d)(\d\d\d)(?!\d)/$1.$2/; # add . on right places
     say "Won: €$prize";
 }else{
-    say "No matches";
+    say "No prizes.";
 }
 
 sub do_GET {
